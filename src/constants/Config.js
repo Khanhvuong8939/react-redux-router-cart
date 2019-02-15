7652a2a1be24cdfdcba07980fb151cd35cf5bb59
vuong.tk <vuong.tk@gnt.com.vn>
2019-02-15 11:39:43 +0700

    fix config bug

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,2 +1,2 @@
 export const API_URL = 'http://localhost:3000';//local
-export const MOCK_API = 'http://5c663c1719df280014b6279b.mockapi.io/api/v1';//local
+export const MOCK_API = 'https://5c663c1719df280014b6279b.mockapi.io/api/v1';//mock Api
